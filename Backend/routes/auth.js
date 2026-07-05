6fcae7782f76d2f5e5bb6bca76db97695b77f9f7
Reem Kareem <reemk6280@gmail.com>
2026-07-05 23:53:28 +0300

    Update auth.js

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('./models/User');
 
 // --- Middleware to protect routes and verify JWT Token ---
 const authMiddleware = async (req, res, next) => {
